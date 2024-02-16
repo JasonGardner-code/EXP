d55689378b862518574b0860fbbacc6335d7afe2
iAmTheRealJason <51766718+JasonGardner-code@users.noreply.github.com>
2024-02-16 10:48:39 -0800

    Update 32emoji.py

--- 32emoji.py
+++ 32emoji.py
@@ -1,3 +1,4 @@
+import random 
 # Generating 32 SSIDs, each 7 characters long, using only emojis
 # Creating a list of emojis to choose from
 emoji_list = [
@@ -10,4 +11,4 @@ emoji_list = [
 # Generating SSIDs by randomly selecting emojis and concatenating them to form a string of 7 emojis
 ssids_emoji = ['"' + ''.join(random.choices(emoji_list, k=7)) + '"' for _ in range(32)]
 ssids_emoji_row = ' '.join(ssids_emoji)
-ssids_emoji_row
+print(ssids_emoji_row)
